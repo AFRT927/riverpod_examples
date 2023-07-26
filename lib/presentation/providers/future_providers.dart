d8552a37f6ddcc07f6c8a420d96b1d17f34832d5
Andres <andrese-rios@utp.edu.co>
2023-07-26 15:33:29 -0500

    uso del modificador Family para pasarle un argumento al State provider y asi poder acuatizarlo. De esta manera se logra evitar el uso de un Provider para actualizar un StateProvider, pero para esto se requiere utilizar un Statefull Widget

--- lib/presentation/providers/future_providers.dart
+++ lib/presentation/providers/future_providers.dart
@@ -11,3 +11,9 @@ final pokemonNameProvider = FutureProvider.autoDispose<String>((ref) async {
 final pokemonIdProvider = StateProvider.autoDispose<int>((ref){
 return 1;
 });
+
+
+final pokemonNameProviderF = FutureProvider.family<String, int>((ref, pokemonId) async {
+  final pokemonName = await PokemonInformation.getPokemonName(pokemonId);
+  return pokemonName;
+});
--- lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
+++ lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
 
-class FamilyFutureScreen extends StatelessWidget {
+class FamilyFutureScreen extends ConsumerStatefulWidget {
   const FamilyFutureScreen({super.key});
 
   @override
+  ConsumerState<FamilyFutureScreen> createState() => FamilyFutureScreenState();
+}
+
+class FamilyFutureScreenState extends ConsumerState<FamilyFutureScreen> {
+
+  /**
+   * se utiliza un statefull widget justamente por que NO
+   * es posible cambiar el valor del pokemonId sin usar 
+   * un SetState
+   */
+
+  int pokemonId = 1;
+
+  @override
   Widget build(BuildContext context) {
+
+    final  pokemonAsync = ref.watch( pokemonNameProviderF(pokemonId) );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Family Future Provider'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: pokemonAsync.when(
+          data: (data) => Text(data), 
+          error: (_,__) => const Text('No se encontro el pokemon'), 
+          loading: () => const CircularProgressIndicator()),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon( Icons.refresh ),
-        onPressed: () {  },
+        onPressed: () {  
+            setState(() {
+              pokemonId++;
+            });
+        }
       ),
     );
   }
