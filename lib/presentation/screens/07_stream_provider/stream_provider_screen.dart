ac9ddc89a6a2d9cf04ca9d47dbce7c74ac8e4d54
Andres <andrese-rios@utp.edu.co>
2023-07-26 16:25:53 -0500

    aplicacion del StreamProvider para manejar estados del tipo <Flujo de datos>

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
 
 class StreamProviderScreen extends StatelessWidget {
@@ -10,6 +12,34 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: StreamView(),
     );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+   // ojo: "usersStream" es un valor asincrono
+   final usersStream = ref.watch(usersInChatProvider);
+
+    if (!usersStream.hasValue) {
+       return Center(
+        child: CircularProgressIndicator(),
+       );
+    }
+
+    return ListView.builder(
+      itemCount: usersStream.value!.length ?? 0,
+      itemBuilder: (BuildContext context, i) {
+        final name = usersStream.value![i];
+        return ListTile(
+          title: Text(name),
+        );
+      }
+      );
   }
 }
